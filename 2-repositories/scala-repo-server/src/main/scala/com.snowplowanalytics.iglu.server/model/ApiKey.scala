da5dbbf0a93c5bf667250705c0e83de5c42c9c85
Fred Blundun <fred@snowplowanalytics.com>
2015-11-19 13:22:06 +0000

    Scala Repo Server: make it possible to create * auth keys (close #72)

--- 2-repositories/scala-repo-server/src/main/scala/com.snowplowanalytics.iglu.server/model/ApiKey.scala
+++ 2-repositories/scala-repo-server/src/main/scala/com.snowplowanalytics.iglu.server/model/ApiKey.scala
@@ -125,8 +125,8 @@ class ApiKeyDAO(val db: Database) extends DAO {
       apiKeys
         .map(_.vendorPrefix)
         .list
-        .filter(o => o.startsWith(vendorPrefix) || vendorPrefix.startsWith(o) ||
-          o == vendorPrefix)
+        .filter(o => (o.startsWith(vendorPrefix) || vendorPrefix.startsWith(o) ||
+          o == vendorPrefix) && o != "*")
         .length == 0
     }
 
