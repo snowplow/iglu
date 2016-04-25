033221f9b030cb230d0c574a04a0164caaab9178
Anton Parkhomenko <mailbox@chuwy.me>
2016-04-25 14:02:18 +0300

    Repo Syncer: fixed assumption that any string with "jsonschema" contains JSON Schema (closes #137)

--- 0-common/repo-syncer/sync.bash
+++ 0-common/repo-syncer/sync.bash
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2015 Snowplow Analytics Ltd. All rights reserved.
+# Copyright (c) 2015-2016 Snowplow Analytics Ltd. All rights reserved.
 #
 # This program is licensed to you under the Apache License Version 2.0, and
 # you may not use this file except in compliance with the Apache License
@@ -26,11 +26,30 @@ host=$1;
 apikey=$2;
 schemafolder=$3;
 
-for schemapath in $(find $schemafolder -type f | grep 'jsonschema'); do
+# Function sending POST request with first argument
+upload () {
   destination="$host/api/schemas/$(
     # Keep the last 4 slash-separated components of the filename
-    echo $schemapath | awk -F '/' '{print $(NF-3)"/"$(NF-2)"/"$(NF-1)"/"$(NF)}';
+    echo $1 | awk -F '/' '{print $(NF-3)"/"$(NF-2)"/"$(NF-1)"/"$(NF)}';
   )";
-  echo "\nUploading schema in file '$schemapath' to endpoint '$destination'";
-  curl "${destination}?isPublic=true" -XPUT -d @$schemapath -H "apikey: $apikey";
+  echo "\nUploading schema in file '$1' to endpoint '$destination'";
+  curl "${destination}?isPublic=true" -XPOST -d @$1 -H "apikey: $apikey";
+}
+
+# Predicate checking if first argument contains `jsonschema` as its format
+isJsonSchema () {
+  #         1. VENDOR       2. NAME        3. FORMAT      4. SCHEMAVER
+  [[ $1 =~ ([a-zA-Z._-]+)\/([a-zA-Z_-]+)\/([a-zA-Z_-]+)\/([1-9][0-9]*-[0-9]*-[0-9]*) ]]
+  if [[ ${BASH_REMATCH[3]} = "jsonschema" ]]; then
+    return 0;
+  else
+    return 1;
+  fi
+}
+
+for schemapath in $(find $schemafolder -type f); do
+  if isJsonSchema $schemapath; then
+    upload $schemapath;
+  fi
 done;
+
