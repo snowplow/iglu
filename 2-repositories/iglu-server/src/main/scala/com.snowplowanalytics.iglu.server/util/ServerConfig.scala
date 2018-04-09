b6a719ab57a9a52c6184be085945e4c4a10c714e
Oguzhan Unlu <cengoguzhanunlu@gmail.com>
2018-04-09 18:32:06 +0300

    Server: add baseHost to configuration (close #362)

--- 2-repositories/iglu-server/src/main/scala/com.snowplowanalytics.iglu.server/util/ServerConfig.scala
+++ 2-repositories/iglu-server/src/main/scala/com.snowplowanalytics.iglu.server/util/ServerConfig.scala
@@ -31,6 +31,8 @@ case class ServerConfig(config: Config) {
 
   //Interface on which the server will be running
   val interface = env("IGLU_INTERFACE") getOrElse config.getString("repo-server.interface")
+  // baseUrl of Swagger UI
+  val baseHost = env("IGLU_SWAGGER_UI_BASE_HOST") getOrElse config.getString("repo-server.baseHost")
   //Port on which the server will be running
   val port = env("IGLU_PORT").map(_.toInt) getOrElse config.getInt("repo-server.port")
 
