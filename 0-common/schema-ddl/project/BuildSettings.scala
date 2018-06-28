4445719136a96b2e7eb3481fc028f8a52d3b3760
szareiangm <szareian@globeandmail.com>
2018-06-28 18:41:00 -0400

    Schema DDL: bump SBT to 1.1.6 (close #334)

--- 0-common/schema-ddl/project/BuildSettings.scala
+++ 0-common/schema-ddl/project/BuildSettings.scala
@@ -1,18 +1,16 @@
-/*
- * Copyright (c) 2012-2016 Snowplow Analytics Ltd. All rights reserved.
+/**
+ * Copyright (c) 2014-2017 Snowplow Analytics Ltd. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0,
- * and you may not use this file except in compliance with the
- * Apache License Version 2.0.
- * You may obtain a copy of the Apache License Version 2.0 at
- * http://www.apache.org/licenses/LICENSE-2.0.
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
  *
  * Unless required by applicable law or agreed to in writing,
- * software distributed under the Apache License Version 2.0 is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied.  See the Apache License Version 2.0 for the specific
- * language governing permissions and limitations there under.
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
+
 import bintray.BintrayPlugin._
 import bintray.BintrayKeys._
 import sbt._
@@ -20,29 +18,25 @@ import Keys._
 
 object BuildSettings {
 
-  // Basic settings for our app
-  lazy val basicSettings = Seq[Setting[_]](
-    organization          :=  "com.snowplowanalytics",
-    version               :=  "0.7.0",
-    description           :=  "Set of Abstract Syntax Trees for various DDL and Schema formats",
-    scalaVersion          :=  "2.12.4",
-    crossScalaVersions    :=  Seq("2.11.12", "2.12.4"),
-    scalacOptions         :=  Seq(
-      "-deprecation",
-      "-encoding", "UTF-8",
-      "-feature",
-      "-unchecked",
-      "-Ywarn-unused-import",
-      "-Ywarn-nullary-unit",
-      "-Xfatal-warnings",
-      "-Xlint",
-      "-language:higherKinds",
-      "-Ypartial-unification",
-      "-Xfuture"),
-    scalacOptions in (Compile, doc) ++= Seq(
-      "-no-link-warnings" // Suppresses problems with Scaladoc @throws links
-    ),
-    scalacOptions in Test :=  Seq("-Yrangepos")
+  lazy val compilerOptions = Seq(
+    "-deprecation",
+    "-encoding", "UTF-8",
+    "-feature",
+    "-language:existentials",
+    "-language:higherKinds",
+    "-language:implicitConversions",
+    "-unchecked",
+    "-Yno-adapted-args",
+    "-Ywarn-dead-code",
+    "-Ywarn-numeric-widen",
+    "-Ywarn-unused-import",
+    "-Xfuture",
+    "-Xlint"
+  )
+
+  lazy val javaCompilerOptions = Seq(
+    "-source", "1.8",
+    "-target", "1.8"
   )
 
   // Publish settings
@@ -60,14 +54,13 @@ object BuildSettings {
     pomExtra := (
       <developers>
         <developer>
-        <name>Snowplow Analytics Ltd</name>
-        <email>support@snowplowanalytics.com</email>
-        <organization>Snowplow Analytics Ltd</organization>
-        <organizationUrl>http://snowplowanalytics.com</organizationUrl>
-          </developer>
-        </developers>)
+          <name>Snowplow Analytics Ltd</name>
+          <email>support@snowplowanalytics.com</email>
+          <organization>Snowplow Analytics Ltd</organization>
+          <organizationUrl>http://snowplowanalytics.com</organizationUrl>
+        </developer>
+      </developers>)
   )
 
-
-  lazy val buildSettings = basicSettings ++ publishSettings ++ mavenCentralExtras
+  lazy val buildSettings =  publishSettings ++ mavenCentralExtras
 }
