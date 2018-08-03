8babc22fde49ac65a2fb5f298af6b18b93b22424
Oguzhan Unlu <cengoguzhanunlu@gmail.com>
2018-08-03 12:45:48 +0300

    igluctl: add static deploy command (closes #375)

--- 0-common/igluctl/src/main/scala/com.snowplowanalytics.iglu/ctl/Command.scala
+++ 0-common/igluctl/src/main/scala/com.snowplowanalytics.iglu/ctl/Command.scala
@@ -36,6 +36,9 @@ case class Command(
   command:         Option[String]  = None,
   input:           Option[File]    = None,
 
+  // deploy
+  config:          Option[File]    = None,
+
   // ddl
   output:          Option[File]    = None,
   db:              String          = "redshift",
@@ -66,6 +69,7 @@ case class Command(
   region:          Option[String]  = None
 ) {
   def toCommand: Option[Command.CtlCommand] = command match {
+    case Some("static deploy") => Some(DeployCommand(config.get))
     case Some("static generate") => Some(
       GenerateCommand(input.get, output.getOrElse(new File(".")), db, withJsonPaths, rawMode, schema, varcharSize, splitProduct, noHeader, force, owner))
     case Some("static push") =>
@@ -92,7 +96,7 @@ object Command {
   }
 
   implicit val lintersRead: scopt.Read[List[Linter]] = scopt.Read.reads { s =>
-    LintCommand.validateSkippedLinters(s) match {
+    LintCommand.skipLinters(s) match {
       case Left(err) => throw new IllegalArgumentException(err)
       case Right(linters) => linters
     }
@@ -125,7 +129,6 @@ object Command {
       .action { (_, c) => c.copy(command = Some("static")) }
       .text("Static Iglu generator\n")
       .children(
-
         cmd("generate")
           .action { (_, c) => c.copy(command = c.command.map(_ + " generate")) }
           .text("Generate DDL out of JSON Schema\n")
@@ -185,6 +188,16 @@ object Command {
                 failure("Options --with-json-paths and --split-product cannot be used together")
               case _ => success
             }
+          ),
+
+        cmd("deploy")
+          .action(subcommand("deploy"))
+          .text("Perform usual schema workflow at once\n")
+          .children(
+
+            arg[File]("config") required()
+              action { (x, c) => c.copy(config = Some(x))}
+              text "Path to configuration file\n"
           ),
 
         cmd("push")
@@ -280,7 +293,7 @@ object Command {
                 "\t\t\t   numericMinMax         : Check that schema with numeric type contains both minimum and maximum properties\n" +
                 "\t\t\t   stringLength          : Check that schema with string type contains maxLength property or other ways to extract max length\n" +
                 "\t\t\t   optionalNull          : Check that non-required fields have null type\n" +
-                "\t\t\t   description           : Check that property contains description\n"
+                "\t\t\t   description           : Check that property contains description"
       )
   }
 }
