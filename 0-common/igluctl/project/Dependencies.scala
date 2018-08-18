756ab59f39c5d2023303494494c0336e5a4acd5e
Anton Parkhomenko <mailbox@chuwy.me>
2018-08-19 01:52:43 +0700

    bump

--- 0-common/igluctl/project/Dependencies.scala
+++ 0-common/igluctl/project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val jaxbImpl         = "2.3.0"
     val activation       = "1.1.1"
     // Scala
-    val schemaddl        = "0.8.0"
+    val schemaddl        = "0.8.0-M5"
     val igluClient       = "0.5.0"
     val scopt            = "3.5.0"
     val scalajHttp       = "2.3.0"
