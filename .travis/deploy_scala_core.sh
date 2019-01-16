6608ccd685d7140dab82a903478d1b1abba28f74
Anton Parkhomenko <mailbox@chuwy.me>
2019-01-17 02:10:19 +0700

    Common: extend Travis timeout to 30 minutes (close #471)

--- .travis/deploy_scala_core.sh
+++ .travis/deploy_scala_core.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e
-
+set -e 
 tag=$1
 
 project="scala-core/"
@@ -9,6 +8,8 @@ project_len=${#project}
 
 cicd=${tag:0:${project_len}}
 release=${tag:${project_len}}
+
+limit=30    # Timeout to extend Travis build time (from 10 mins)
 
 if [ "${cicd}" == "${project}" ]; then
     if [ "${release}" == "" ]; then
@@ -33,6 +34,21 @@ cd "${TRAVIS_BUILD_DIR}/0-common/scala-core"
 
 project_version=$(sbt version -Dsbt.log.noformat=true | tail -n 1 | perl -ne 'print $1 if /(\d+\.\d+[^\r\n]*)/')
 
+function travis_wait {
+  minutes=0
+  while kill -0 $! >/dev/null 2>&1; do
+    echo -n -e " \b" # never leave evidences!
+  
+    if [ $minutes == $limit ]; then
+      break;
+    fi
+  
+    minutes=$((minutes+1))
+  
+    sleep 60
+  done
+}
+
 if [ "${project_version}" == "${release}" ]; then
     # igluCore
     echo "DEPLOY: testing iglu-core..."
@@ -40,21 +56,26 @@ if [ "${project_version}" == "${release}" ]; then
     echo "DEPLOY: publishing iglu-core..."
     sbt +publish
     echo "DEPLOY: publishing iglu-core to Maven Central..."
-    sbt +bintraySyncMavenCentral
+    sbt +bintraySyncMavenCentral &
+    travis_wait
+
     # igluCoreCirce
     echo "DEPLOY: testing iglu-core-circe..."
     sbt "project igluCoreCirce" +test --warn
     echo "DEPLOY: publishing iglu-core-circe..."
     sbt "project igluCoreCirce" +publish
     echo "DEPLOY: publishing iglu-core-circe to Maven Central..."
-    sbt "project igluCoreCirce" +bintraySyncMavenCentral
+    sbt "project igluCoreCirce" +bintraySyncMavenCentral &
+    travis_wait
+
     # igluCoreJson4s
     echo "DEPLOY: testing iglu-core-json4s..."
     sbt "project igluCoreJson4s" +test --warn
     echo "DEPLOY: publishing iglu-core-json4s..."
     sbt "project igluCoreJson4s" +publish
     echo "DEPLOY: publishing iglu-core-json4s to Maven Central..."
-    sbt "project igluCoreJson4s" +bintraySyncMavenCentral
+    sbt "project igluCoreJson4s" +bintraySyncMavenCentral &
+    travis_wait
     echo "DEPLOY: Iglu Core deployed..."
 
 
